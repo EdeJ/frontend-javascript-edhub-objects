20c44a5b1b972c852c6dbc1155f247a7b3cc7cf8
Emiel de Jong <emieldejong@xs4all.nl>
2020-10-06 12:24:59 +0200

    finished

--- objects.js
+++ objects.js
@@ -11,16 +11,20 @@ const product = {
 };
 
 // 1a. Log de naam van de serie in de console.
+console.log('productnaam: ' + product.productName);
 
 
 // 1b. Verander de kleur van het product in "Roze" en log het object in de console
+product.color = 'Roze';
+console.log('produkt kleur: ' + product.color);
 
 
 // 1c. Log de tweede materiaalsoort ("kunststof") in de console
+console.log('materialen: ' + product.material);
 
 
 // 1d. Log de afmetingen (b-h-d) in de console
-
+console.log('afmeting: ' + product['b-h-d']);
 
 
 /* Opdracht 2 */
@@ -29,6 +33,10 @@ const product = {
 // ---- Verwachte uitkomst:
 // "De Smeg Waterkoker is gemaakt van roestvrijstaal en kunststof"
 
+function printProduct(prod) {
+    return 'De ' + prod.productName + ' is gemaakt van ' + prod.material[0] + ' en ' + prod.material[1];
+}
+console.log(printProduct(product));
 
 
 /* Opdracht 3 */
@@ -36,6 +44,18 @@ const product = {
 // leeftijd (getal) en geboortedatum (date object). De datum hoeft niet geformat te worden.
 // Log dit in de console.
 
+const me = {
+    firstName: 'Emiel',
+    lastName: 'de Jong',
+    age: 45,
+    dateOfBirth: new Date('Nov 11, 1975')
+}
+for (let propName in me) {
+    console.log(propName + ': ' + me[propName]);
+}
+
+// of:
+console.log('my Object', me);
 
 
 /* Opdracht */
@@ -44,3 +64,8 @@ const product = {
 // ---- Verwachte uitkomsten:
 // { age: 27 } geeft "Jij bent 27 jaar oud."
 // { age: 56 } geeft "Jij bent 56 jaar oud."
+
+const getAge = (person) => {
+    return 'Ik ben ' + me.age + ' jaar oud.';
+}
+console.log(getAge(me));
